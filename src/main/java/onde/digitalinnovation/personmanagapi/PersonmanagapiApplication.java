a8ddbb0a45d4ccaaf9be71edae04ccc985a537c1
jmarq76 <joaomarques2176@gmail.com>
2021-04-30 16:37:19 -0300

    Added project for person management api

---
+++ src/main/java/onde/digitalinnovation/personmanagapi/PersonmanagapiApplication.java
@@ -0,0 +1,13 @@
+package onde.digitalinnovation.personmanagapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonmanagapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonmanagapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onde/digitalinnovation/personmanagapi/PersonmanagapiApplicationTests.java
@@ -0,0 +1,13 @@
+package onde.digitalinnovation.personmanagapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonmanagapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
